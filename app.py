53da1208765b15d8f260e983497a61e0dbbdc420
Ubuntu <ubuntu@ip-172-31-83-214.ec2.internal>
2019-10-11 22:32:07 +0000

    still trying to fix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, request, url_for
 import os 
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
