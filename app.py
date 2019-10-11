0a6f30ff9ab6dba4ea37d6152a9c12553a2cc167
Ubuntu <ubuntu@ip-172-31-83-214.ec2.internal>
2019-10-11 21:14:12 +0000

    IP address and port set

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os 
 
 app=Flask(__name__)
+
 
 @app.route('/')
 def landing_page():
     return 'Hello World'
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)    
