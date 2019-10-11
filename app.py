34a05af9aeca2d243f4f06438385d9312e69ddd0
Ubuntu <ubuntu@ip-172-31-83-214.ec2.internal>
2019-10-11 21:57:48 +0000

    new route added for landing page

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 import os 
+from flask_pymongo import PyMongo, render_template, redirect, request, url_for
+from bson.objectid import ObjectId
+
 
 app=Flask(__name__)
 
+app.config["MONGO_DBNAME"]='photo_locations'
+app.cofig['MONGO_URI']='mongodb+srv://nick:n1ckUser@myfirstcluster-mbpma.mongodb.net/photo_locations?retryWrites=true&w=majority'
+mongo=PyMongo(app)
 
 @app.route('/')
-def landing_page():
+def index():
     return 'Hello World'
+    
+@app.route('/landing')
+def landing():
+    return render_template('landing.html', entries=mongo.db.photo_locations.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)    
