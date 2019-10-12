117a806cbc895295925ba02cdd69020c6858a9e4
Ubuntu <ubuntu@ip-172-31-83-214.ec2.internal>
2019-10-12 11:07:15 +0000

    typo corrected

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bson.objectid import ObjectId
 app=Flask(__name__)
 
 app.config["MONGO_DBNAME"]='photo_locations'
-app.cofig['MONGO_URI']='mongodb+srv://nick:n1ckUser@myfirstcluster-mbpma.mongodb.net/photo_locations?retryWrites=true&w=majority'
+app.config['MONGO_URI']='mongodb+srv://nick:n1ckUser@myfirstcluster-mbpma.mongodb.net/photo_locations?retryWrites=true&w=majority'
 mongo=PyMongo(app)
 
 @app.route('/')
